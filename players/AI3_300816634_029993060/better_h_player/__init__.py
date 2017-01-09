b4af49fd27bcc34db41fe06150e17ee22d7584eb
Unguliant <ungulient@gmail.com>
2017-01-10 01:48:43 +0200

    2

--- players/AI3_300816634_029993060/better_h_player/__init__.py
+++ players/AI3_300816634_029993060/better_h_player/__init__.py
@@ -7,9 +7,10 @@ import abstract
 import players.simple_player as simple_player
 from utils import INFINITY
 from checkers.consts import EM, BK, RK, RP, BP, PAWN_COLOR, KING_COLOR, OPPONENT_COLOR, MAX_TURNS_NO_JUMP, \
-    OPPONENT_COLORS
+    OPPONENT_COLORS, BACK_ROW
 from collections import defaultdict
 from checkers.moves import GameMove, PAWN_SINGLE_MOVES, KING_SINGLE_MOVES, PAWN_CAPTURE_MOVES, KING_CAPTURE_MOVES
+from math import hypot
 
 #===============================================================================
 # Globals
@@ -23,31 +24,31 @@ KING_MOVE_WEIGHT = 2
 KING_LOC_WEIGHT = 1.3
 LOC_BONUS = {
     RP: {
-        (7, 0): 0, (7, 1): 7, (7, 2): 0, (7, 3): 7, (7, 4): 0, (7, 5): 7, (7, 6): 0, (7, 7): 7,
-        (6, 0): 6, (6, 1): 0, (6, 2): 6, (6, 3): 0, (6, 4): 6, (6, 5): 0, (6, 6): 6, (6, 7): 0,
-        (5, 0): 0, (5, 1): 5, (5, 2): 0, (5, 3): 5, (5, 4): 0, (5, 5): 5, (5, 6): 0, (5, 7): 2,
-        (4, 0): 2, (4, 1): 0, (4, 2): 4, (4, 3): 0, (4, 4): 4, (4, 5): 0, (4, 6): 4, (4, 7): 0,
-        (3, 0): 0, (3, 1): 3, (3, 2): 0, (3, 3): 3, (3, 4): 0, (3, 5): 3, (3, 6): 0, (3, 7): 3,
-        (2, 0): 2, (2, 1): 0, (2, 2): 2, (2, 3): 0, (2, 4): 2, (2, 5): 0, (2, 6): 2, (2, 7): 0,
-        (1, 0): 0, (1, 1): 3, (1, 2): 0, (1, 3): 3, (1, 4): 0, (1, 5): 3, (1, 6): 0, (1, 7): 3,
         (0, 0): 4, (0, 1): 0, (0, 2): 4, (0, 3): 0, (0, 4): 4, (0, 5): 0, (0, 6): 4, (0, 7): 0,
+        (1, 0): 0, (1, 1): 3, (1, 2): 0, (1, 3): 3, (1, 4): 0, (1, 5): 3, (1, 6): 0, (1, 7): 3,
+        (2, 0): 2, (2, 1): 0, (2, 2): 2, (2, 3): 0, (2, 4): 2, (2, 5): 0, (2, 6): 2, (2, 7): 0,
+        (3, 0): 0, (3, 1): 3, (3, 2): 0, (3, 3): 3, (3, 4): 0, (3, 5): 3, (3, 6): 0, (3, 7): 3,
+        (4, 0): 2, (4, 1): 0, (4, 2): 4, (4, 3): 0, (4, 4): 4, (4, 5): 0, (4, 6): 4, (4, 7): 0,
+        (5, 0): 0, (5, 1): 5, (5, 2): 0, (5, 3): 5, (5, 4): 0, (5, 5): 5, (5, 6): 0, (5, 7): 2,
+        (6, 0): 6, (6, 1): 0, (6, 2): 6, (6, 3): 0, (6, 4): 6, (6, 5): 0, (6, 6): 6, (6, 7): 0,
+        (7, 0): 0, (7, 1): 7, (7, 2): 0, (7, 3): 7, (7, 4): 0, (7, 5): 7, (7, 6): 0, (7, 7): 7,
     },
     RK: {
-        (7, 0): 0, (7, 1): 1, (7, 2): 0, (7, 3): 1, (7, 4): 0, (7, 5): 1, (7, 6): 0, (7, 7): 1,
-        (6, 0): 1, (6, 1): 0, (6, 2): 3, (6, 3): 0, (6, 4): 3, (6, 5): 0, (6, 6): 3, (6, 7): 0,
-        (5, 0): 0, (5, 1): 5, (5, 2): 0, (5, 3): 5, (5, 4): 0, (5, 5): 5, (5, 6): 0, (5, 7): 1,
-        (4, 0): 1, (4, 1): 0, (4, 2): 7, (4, 3): 0, (4, 4): 7, (4, 5): 0, (4, 6): 7, (4, 7): 0,
-        (3, 0): 0, (3, 1): 7, (3, 2): 0, (3, 3): 7, (3, 4): 0, (3, 5): 7, (3, 6): 0, (3, 7): 1,
-        (2, 0): 1, (2, 1): 0, (2, 2): 5, (2, 3): 0, (2, 4): 5, (2, 5): 0, (2, 6): 5, (2, 7): 0,
-        (1, 0): 0, (1, 1): 3, (1, 2): 0, (1, 3): 3, (1, 4): 0, (1, 5): 3, (1, 6): 0, (1, 7): 1,
         (0, 0): 1, (0, 1): 0, (0, 2): 1, (0, 3): 0, (0, 4): 1, (0, 5): 0, (0, 6): 1, (0, 7): 0,
+        (1, 0): 0, (1, 1): 3, (1, 2): 0, (1, 3): 3, (1, 4): 0, (1, 5): 3, (1, 6): 0, (1, 7): 1,
+        (2, 0): 1, (2, 1): 0, (2, 2): 5, (2, 3): 0, (2, 4): 5, (2, 5): 0, (2, 6): 5, (2, 7): 0,
+        (3, 0): 0, (3, 1): 7, (3, 2): 0, (3, 3): 7, (3, 4): 0, (3, 5): 7, (3, 6): 0, (3, 7): 1,
+        (4, 0): 1, (4, 1): 0, (4, 2): 7, (4, 3): 0, (4, 4): 7, (4, 5): 0, (4, 6): 7, (4, 7): 0,
+        (5, 0): 0, (5, 1): 5, (5, 2): 0, (5, 3): 5, (5, 4): 0, (5, 5): 5, (5, 6): 0, (5, 7): 1,
+        (6, 0): 1, (6, 1): 0, (6, 2): 3, (6, 3): 0, (6, 4): 3, (6, 5): 0, (6, 6): 3, (6, 7): 0,
+        (7, 0): 0, (7, 1): 1, (7, 2): 0, (7, 3): 1, (7, 4): 0, (7, 5): 1, (7, 6): 0, (7, 7): 1,
     }
 }
 LOC_BONUS[BP] = {(i, j): (LOC_BONUS[RP])[(7 - i, 7 - j)]
                  for (i, j) in LOC_BONUS[RP]}
 LOC_BONUS[BK] = {(i, j): (LOC_BONUS[RK])[(7 - i, 7 - j)]
                  for (i, j) in LOC_BONUS[RK]}
-MAX_DISTANCE = 7
+DISTANCE_BONUS = 10
 DISTANCE_WEIGHT = 10
 
 #===============================================================================
@@ -105,31 +106,42 @@ class Player(simple_player.Player):
         my_location_bonus = 0
         op_location_bonus = 0
 
-        if state.turns_since_last_jump > 12 or (my_u < 8 and op_u < 8):
-            # endgame
+        if (state.turns_since_last_jump > 12 or
+                (piece_counts[PAWN_COLOR[self.color]] + piece_counts[KING_COLOR[self.color]] < 8
+                 and piece_counts[PAWN_COLOR[opponent_color]] + piece_counts[KING_COLOR[opponent_color]] < 8)):
+            # endgame - try to minimize distance
             if piece_counts[KING_COLOR[self.color]] > 0:
                 my_kings_average_distance = find_kings_min_distances_sum(self.color, unit_location) / piece_counts[KING_COLOR[self.color]]
                 if my_kings_average_distance > 0:
                     my_kings_distance_bonus = ((state.turns_since_last_jump / MAX_TURNS_NO_JUMP) *
-                                               DISTANCE_WEIGHT * (MAX_DISTANCE - my_kings_average_distance))
+                                               DISTANCE_WEIGHT * (DISTANCE_BONUS - my_kings_average_distance))
 
             if piece_counts[KING_COLOR[opponent_color]] > 0:
                 op_kings_average_distance = find_kings_min_distances_sum(opponent_color, unit_location) / piece_counts[KING_COLOR[opponent_color]]
                 if op_kings_average_distance > 0:
                     op_kings_distance_bonus = ((state.turns_since_last_jump / MAX_TURNS_NO_JUMP) *
-                                               DISTANCE_WEIGHT * (MAX_DISTANCE - op_kings_average_distance))
-        else:
-            # not endgame
-            my_location_bonus = ((PAWN_LOC_WEIGHT * location_bonus[KING_COLOR[self.color]]) +
-                                 (KING_LOC_WEIGHT * location_bonus[KING_COLOR[self.color]]))
-            op_location_bonus = ((PAWN_LOC_WEIGHT * location_bonus[KING_COLOR[opponent_color]]) +
-                                 (KING_LOC_WEIGHT * location_bonus[KING_COLOR[opponent_color]]))
+                                               DISTANCE_WEIGHT * (DISTANCE_BONUS - op_kings_average_distance))
+
+        my_location_bonus = ((PAWN_LOC_WEIGHT * location_bonus[KING_COLOR[self.color]]) +
+                             (KING_LOC_WEIGHT * location_bonus[KING_COLOR[self.color]]))
+        op_location_bonus = ((PAWN_LOC_WEIGHT * location_bonus[KING_COLOR[opponent_color]]) +
+                             (KING_LOC_WEIGHT * location_bonus[KING_COLOR[opponent_color]]))
 
         my_pawn_moves, my_king_moves = calc_all_moves(state, state.curr_player)
         op_pawn_moves, op_king_moves = calc_all_moves(state, opponent_color)
 
+        # mobility
         my_mobility = PAWN_MOVE_WEIGHT * len(my_pawn_moves) + KING_MOVE_WEIGHT * len(my_king_moves)
         op_mobility = PAWN_MOVE_WEIGHT * len(op_pawn_moves) + KING_MOVE_WEIGHT * len(op_king_moves)
+
+        # runaway checkers
+        # for move in my_pawn_moves:
+        #     row_distance = abs(move.target_loc[0] - BACK_ROW[self.color])
+        #     if (row_distance == 0 or
+        #             (row_distance == 1 and (st)))
+        #
+        # for move in op_pawn_moves:
+        # for all available pawn moves - if target_loc passed enemy lines and clear line to kinghood, add bonus
 
         return ((my_u + my_mobility + my_location_bonus + my_kings_distance_bonus) -
                 (op_u + op_mobility + op_location_bonus + op_kings_distance_bonus))
@@ -185,10 +197,7 @@ def calc_all_moves(state, player):
 
 
 def distance(loc1, loc2):
-    if loc2[0] > loc1[0]:
-        return abs(loc1[1] - loc2[1])
-    else:
-        return abs(loc1[0] - loc2[0])
+    return hypot(loc2[0] - loc1[0], loc2[1] - loc1[1])
 
 
 def find_kings_min_distances_sum(player, unit_location):
