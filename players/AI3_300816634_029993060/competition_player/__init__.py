a14f9271ced815ed3b04298305de69af9f95a5a7
Unguliant <ungulient@gmail.com>
2017-01-10 22:44:46 +0200

    Added Competition Player

---
+++ players/AI3_300816634_029993060/competition_player/__init__.py
@@ -0,0 +1,19 @@
+
+#===============================================================================
+# Imports
+#===============================================================================
+
+import abstract
+import players.AI3_300816634_029993060.improved_better_h_player as ibh
+
+
+#===============================================================================
+# Player
+#===============================================================================
+
+class Player(ibh.Player):
+    def __init__(self, setup_time, player_color, time_per_k_turns, k):
+        super().__init__(setup_time, player_color, time_per_k_turns, k)
+
+    def __repr__(self):
+        return '{} {}'.format(abstract.AbstractPlayer.__repr__(self), 'competition')
